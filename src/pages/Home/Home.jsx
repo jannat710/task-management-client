d21504e1cfb201a5cdaaf6a79ad37490bcd95aa9
e <jannatul0040@gmail.com>
2023-12-22 01:30:00 +0600

    added user category

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import UsersCategory from "./UsersCategory";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <UsersCategory></UsersCategory>
           this is home  
         </div>
     );
---
+++ src/pages/Home/UsersCategory.jsx
@@ -0,0 +1,55 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import p1 from '../../assets/userscategory/bankers.jpg'
+import p2 from '../../assets/userscategory/corporate.jpg'
+import p3 from '../../assets/userscategory/developers.jpg'
+import p4 from '../../assets/userscategory/professionals.jpg'
+import p5 from '../../assets/userscategory/teachers.jpg'
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// import required modules
+import { FreeMode, Pagination } from 'swiper/modules';
+
+const UsersCategory = () => {
+    return (
+        <div className='py-10'>
+            <h1 className='text-4xl font-semibold pb-5'>Discover the Diverse Users <br></br>of Our Website!</h1>
+            <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <img src={p1} alt="" />
+            <h3 className='text-4xl uppercase text-center text-white -mt-16'>Bankers</h3>
+            </SwiperSlide>
+        <SwiperSlide>
+            <img src={p2} alt="" />
+            <h3 className='text-4xl uppercase text-center text-white -mt-16'>Corporate</h3>
+            </SwiperSlide>
+        <SwiperSlide>
+            <img src={p3} alt="" />
+            <h3 className='text-4xl uppercase text-center text-white -mt-16'>Developers</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={p4} alt="" />
+            <h3 className='text-4xl uppercase text-center text-white -mt-16'>Proffessionals</h3>
+            </SwiperSlide>
+        <SwiperSlide>
+            <img src={p5} alt="" />
+            <h3 className='text-4xl uppercase text-center text-white -mt-16'>Teachers</h3>
+            </SwiperSlide>
+      </Swiper>
+        </div>
+    );
+};
+
+export default UsersCategory;
